9e2690c357904745eff0452de7eea575c58f8de5
Dennis Asplind <skene@hotmail.com>
2023-09-16 12:15:19 +0200

    boiler layout

--- frontend/web-app/app/layout.tsx
+++ frontend/web-app/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import NavBar from './nav/NavBar';
 
 export const metadata = {
   title: 'CarAuctions',
@@ -12,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <NavBar />
+        <main className='container mx-auto px-5 pt-10'>{children}</main>
+      </body>
     </html>
   );
 }
