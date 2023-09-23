f09f42ac2f8d35ac35caf2969402fa2ede8b867e
Dennis Asplind <skene@hotmail.com>
2023-09-23 06:02:59 +0200

    Add NotificationService with SignalR

---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,32 @@
+using MassTransit;
+using NotificationService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+
+  x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+  x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+
+  x.UsingRabbitMq((context, cfg) =>
+  {
+    cfg.Host(builder.Configuration["RabbitMQ:Host"], "/", host =>
+      {
+        host.Username(builder.Configuration.GetValue("RabbitMQ:Username", "guest"));
+        host.Password(builder.Configuration.GetValue("RabbitMQ:Password", "guest"));
+      });
+
+    cfg.ConfigureEndpoints(context);
+  });
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<NotificationHub>("/notifications");
+
+
+app.Run();
