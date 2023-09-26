4fa449de143babb304c5c63e703699c80162b29c
Dennis Asplind <skene@hotmail.com>
2023-09-26 10:35:14 +0200

    cleanup and resolve signalR issue

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -1,31 +1,38 @@
 'use client';
+
 import { useAuctionStore } from '@/hooks/useAuctionStore';
 import { useBidStore } from '@/hooks/useBidStore';
 import { Auction, AuctionFinished, Bid } from '@/types';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { User } from 'next-auth';
-import React, { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
+import React, { ReactNode, useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
 import AuctionCreatedToast from '../components/AuctionCreatedToast';
 import { getDetailedViewData } from '../actions/auctionActions';
 import AuctionFinishedToast from '../components/AuctionFinishedToast';
+
 type Props = {
-  children: React.ReactNode;
+  children: ReactNode;
   user: User | null;
 };
+
 export default function SignalRProvider({ children, user }: Props) {
   const [connection, setConnection] = useState<HubConnection | null>(null);
   const setCurrentPrice = useAuctionStore((state) => state.setCurrentPrice);
   const addBid = useBidStore((state) => state.addBid);
+  const apiUrl =
+    process.env.NODE_ENV === 'production'
+      ? 'https://api.carauctions.com/notifications'
+      : process.env.NEXT_PUBLIC_NOTIFY_URL;
 
   useEffect(() => {
     const newConnection = new HubConnectionBuilder()
-      .withUrl(process.env.NEXT_PUBLIC_NOTIFY_URL!)
+      .withUrl(apiUrl!)
       .withAutomaticReconnect()
       .build();
 
     setConnection(newConnection);
-  }, []);
+  }, [apiUrl]);
 
   useEffect(() => {
     if (connection) {
@@ -72,6 +79,7 @@ export default function SignalRProvider({ children, user }: Props) {
         })
         .catch((error) => console.log(error));
     }
+
     return () => {
       connection?.stop();
     };
