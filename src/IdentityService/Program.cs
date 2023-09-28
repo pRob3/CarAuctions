fda408f5f2882d98a8827b92cba90327fc8de4a5
Dennis Asplind <skene@hotmail.com>
2023-09-28 14:28:58 +0200

    test github actions

--- src/IdentityService/Program.cs
+++ src/IdentityService/Program.cs
@@ -22,7 +22,7 @@ try
         .ConfigureServices()
         .ConfigurePipeline();
 
-    // this seeding is only for the template to bootstrap the DB and users.
+    // this seeding is only for the template to bootstrap the Db and users.
     // in production you will likely want a different approach.
     var retryPolicy = Policy
         .Handle<NpgsqlException>()
